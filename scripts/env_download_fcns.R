4540b1dc18633cbe9b36567a3769182c36b5cdac
lrmccormick <l.r.mccormick12@gmail.com>
2026-06-23 14:44:43 -0700

    Env data: Added full files with coords for Feather Riv stations. Updated main env_download_fcns file.

--- scripts/env_download_fcns.R
+++ scripts/env_download_fcns.R
@@ -210,8 +210,8 @@ end.date.f <- "2025-06-30"
 # set vector of stations
 stations.flow.f <- c("ORF", "GRL", "FSB", "VON")
 stations.wtemp.f <- c("ORF", "FRA", "FTA", "FOW", "GRL", "VON")
-sta.wtemp.f <- "GRL"
-sta.flow.f <- "ORF"
+sta.flow.f <- c("GRL")
+sta.temp.f <- c("FOW")
 
 # use the fcn for flow data
 flow_feather <- download_cdec_flow(
@@ -222,7 +222,7 @@ flow_feather <- download_cdec_flow(
 
 # use the fcn for temp data
 wtemp_feather <- download_cdec_wtemp(
-  sta_list= sta.wtemp.f, #stations.wtemp.f,
+  sta_list= sta.temp.f, #stations.wtemp.f,
   start_date = start.date.f,
   end_date = end.date.f
 )
@@ -258,7 +258,11 @@ VON_env <- full_join(test.flow, test.temp, by = join_by(datetime))
 
 # Write files to data folder
 write_csv(ORF_env, "data_raw/feat_env_ORF.csv")
-write_csv(FSB_flow_data, "data_raw/feat_env_FSB.csv")
 write_csv(GRL_env, "data_raw/feat_env_GRL.csv")
 write_csv(VON_env, "data_raw/feat_env_VON.csv")
+
+write_csv(FOW_wtemp_data, "data_raw/feat_env_FOW_wtemp.csv")
+write_csv(FRA_wtemp_data, "data_raw/feat_env_FRA_wtemp.csv")
+write_csv(FTA_wtemp_data, "data_raw/feat_env_FTA_wtemp.csv")
+write_csv(FSB_flow_data, "data_raw/feat_env_FSB_flow.csv")
 
