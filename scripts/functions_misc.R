ee76134725c1f52b9f12ab1c47a29837bb09d665
ajp374 <ajp374@cornell.edu>
2026-06-26 10:05:49 -0700

    first network plots

--- scripts/functions_misc.R
+++ scripts/functions_misc.R
@@ -11,9 +11,10 @@
 ##                 for min_count), the line between i and j is not plotted.
 ## Outputs: ggplot of river network
 
-library(reshape2); library(igraph); library(ggplot2)
+library(reshape2); library(igraph); library(ggplot2); library(ggrepel)
+#study_i = "FR_Spring_2013"; filter_perc = 5; min_count = 2; min_rec_dist = 1
 
-plot_river_network = function(data, study_i = NULL, filter_perc = 5, min_count = 2, min_rec_dist){
+plot_river_network = function(data, study_i = NULL, filter_perc = 5, min_count = 2, min_rec_dist = 1){
   
   if(!is.null(study_i)){
     data = subset(data, study_id == study_i)
@@ -49,15 +50,18 @@ plot_river_network = function(data, study_i = NULL, filter_perc = 5, min_count =
   recv.dist =dist(recv_info[, "receiver_general_river_km"])
   recv.dist=as.matrix(recv.dist, labels=TRUE)
   colnames(recv.dist) <- rownames(recv.dist) <- recv_info$receiver_general_location 
-  recv.dist[recv.dist < 1] = 0
-  recv.dist[recv.dist > 1] = 1
+  recv.dist[recv.dist < min_rec_dist] = 0
+  recv.dist[recv.dist >= min_rec_dist] = 1
   recv.pairs = reshape2::melt(recv.dist)
   recv.pairs = subset(recv.pairs, value == 0)
 
   idf <- graph_from_data_frame(recv.pairs)
   recv_info = merge(recv_info, stack(components(idf)$membership), by.x = "receiver_general_location", by.y = "ind", all.x = TRUE)
-  colnames(recv_info)[colnames(recv_info)=="values"] = "rec_group"
-  
+  recv_info = recv_info %>% arrange(receiver_general_river_km) %>% 
+              mutate(rec_group = consecutive_id(values)) %>% ungroup()
+  recv_info$values = NULL
+  recv_info$rec_bin = cut(recv_info$receiver_general_latitude, breaks = seq(min(recv_info$receiver_general_latitude)-.1, max(recv_info$receiver_general_latitude)+.1, by = .1))
+
   ## merge back in with full dataset
   data = merge(data, recv_info[,c("receiver_general_location", "rec_group")], by = "receiver_general_location", sort = F)
 
@@ -78,15 +82,32 @@ plot_river_network = function(data, study_i = NULL, filter_perc = 5, min_count =
                            filter(count_next >= min_count) %>%
                            mutate(perc_next = (count_next / sum(count_next)) * 100)
   
-  ## merge receiver info
+  ## filter by % 
   data_recv_sub = subset(data_recv, perc_next > filter_perc)
-  data_recv_sub = merge(data_recv_sub, recv_info[!duplicated(recv_info$rec_group),], by = 'rec_group')
-  data_recv_sub = merge(data_recv_sub, recv_info[!duplicated(recv_info$rec_group),], by.x = 'next_rec_group',
+  
+  ## assign receiver labels for plotting
+  recv_info_sub = subset(recv_info, rec_group %in% unique(data_recv_sub$rec_group) | rec_group %in% unique(data_recv_sub$next_rec_group))
+  recv_bins = recv_info_sub[!duplicated(recv_info_sub$rec_group), ]
+  recv_bins = recv_bins %>% group_by(rec_bin) %>%
+    mutate(plot_x = consecutive_id(receiver_general_longitude),
+           plot_x_sc = scale(plot_x, scale = F)) %>%
+    ungroup()
+  recv_info_sub = merge(recv_info_sub, recv_bins[, c("rec_group", "plot_x", "plot_x_sc")], by = 'rec_group')
+  
+  
+  # merge with receiver info
+  data_recv_sub = merge(data_recv_sub, recv_info_sub[!duplicated(recv_info_sub$rec_group),], by = 'rec_group')
+  data_recv_sub = merge(data_recv_sub, recv_info_sub[!duplicated(recv_info_sub$rec_group),], by.x = 'next_rec_group',
                         by.y = 'rec_group', suffixes = c(".i", '.j'))
 
-  
+  ## most important receivers
+  recv_important = subset(data_recv_sub, count_next > 10 | rec_group == 2) #subset(data_recv_sub, rec_group %in% c(1, 2, max(data_recv_sub$rec_group))| count_next >= sort(data_recv_sub$count_next, decreasing = T)[10])
+  recv_important = subset(recv_info_sub, rec_group %in% unique(recv_important$rec_group) | rec_group %in% unique(recv_important$next_rec_group))
+  recv_important = subset(recv_important, !duplicated(rec_group))
   
   ### Plotting
+  
+  ## by lat/long
   ggplot(data_recv_sub) + 
     geom_segment(aes(x = receiver_general_longitude.i, y = receiver_general_latitude.i,
                      xend = receiver_general_longitude.j, yend = receiver_general_latitude.j,
@@ -96,6 +117,18 @@ plot_river_network = function(data, study_i = NULL, filter_perc = 5, min_count =
     #geom_point(aes(x = receiver_general_longitude.j, y = receiver_general_latitude.j))+
     scale_linewidth_continuous(range = c(0.01, 1), breaks = c(0, 10, 50, 100))+
     geom_point(data = release_info, aes(x = release_longitude, y = release_latitude, color = release_location))+
-    coord_fixed() + theme_classic()              
+    coord_fixed() + theme_classic() 
+  
+  ## simplified network map
+  ggplot(data_recv_sub) + 
+    geom_segment(aes(y = rec_group, x = plot_x_sc.i,
+                     yend = next_rec_group, xend = plot_x_sc.j,
+                     linewidth = perc_next),
+                 arrow = arrow(length = unit(0.2,"cm")))+
+    scale_linewidth_continuous(range = c(0.01, 1), breaks = c(0, 10, 50, 100))+
+    #geom_point(data = release_info, aes(x = release_longitude, y = release_river_km, color = release_location))+
+    geom_text_repel(data = recv_important, aes(y = rec_group, x = plot_x_sc, label = receiver_general_location),
+                    nudge_x = ifelse(recv_important$plot_x_sc>0, 2, -2), color = 'red')+
+    theme_classic() + labs(x = NULL, y = NULL, linewidth = "% of fish")
 
 } 
